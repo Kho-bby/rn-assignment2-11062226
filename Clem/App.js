214cd6a28b9cbcfa4b6eca46d00c2e4ef10868f0
kho_bby <cckonor@st.ug.edu.gh>
2024-05-27 22:33:10 +0000

    Boldd name

--- Clem/App.js
+++ Clem/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 24}}>My name is Clement.</Text>
+      <Text style={{fontSize: 24}}>My name is <Text style={styles.boldText}>Clement</Text>.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#adff2f',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: '24,'
   },
+  boldText:{
+    fontWeight: 'bold',
+  }
 });
